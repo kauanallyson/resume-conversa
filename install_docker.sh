9484f7cb62832179f21d30ab4def5966f908d336
kauanallyson <163926307+kauanallyson@users.noreply.github.com>
2026-03-10 09:20:01 -0300

    add install script for docker

---
+++ install_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install ca-certificates curl gnupg software-properties-common -y
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo docker run hello-world
